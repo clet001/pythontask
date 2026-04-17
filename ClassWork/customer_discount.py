2a35ac66c3212677590dd75cdb8fa2bdc60aca56
clet001 <nwaforclement7@gmail.com>
2026-04-17 10:15:23 +0100

    customer spending

---
+++ ClassWork/customer_discount.py
@@ -0,0 +1,28 @@
+'''To design a program that applies tiered discount based on a customer's total spending in a store
+SOLUTION:
+
+        Use if.. else
+Collect input
+create a variable with for purchases and discounts
+then use if..else to solve it
+then print
+'''
+
+Spending = input(int("What is your spending?"))
+
+purchase = 50000
+
+
+first_discount = 5
+second_discount = 10
+third_discount = 20
+
+if purchase >= 10000
+    print('first_discount'):
+
+if purchase >= 50000
+    print('second_discount'):
+
+if purchase > 50000
+    print('third_discount'):
+
