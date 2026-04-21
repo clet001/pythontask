cc818c3c96a3bbd704ab9a6895deddfee6ab9bdd
clet001 <nwaforclement7@gmail.com>
2026-04-21 09:37:46 +0100

    Nokia Phone

---
+++ Nokia 3310/nokia_3310.py
@@ -0,0 +1,81 @@
+print("WELCOME TO NOKIA 3310")
+
+
+print("WELCOME TO NOKIA 3310")
+
+print("Enter 1 - PhoneBook")
+print("Enter 2 - Messages")
+print("Enter 3 - Chats")
+print("Enter 4 - Call Register")
+print("Enter 5 - Tones")
+print("Enter 6 - Settings")
+print("Enter 7 - Call Divert")
+print("Enter 8 - Games")
+print("Enter 9 - Calculator")
+print("Enter 10 - Reminders")
+print("Enter 11 - Clock")
+print("Enter 12 - Profiles")
+print("Enter 13 - SIM Services")
+print("Enter 0 - Exit")
+print()
+
+#for user_choice in range(1,14):
+user_choice = int(input("Input Choice: "))
+
+if user_choice == 1:
+    print("Enter 1 - Search")
+    print("Enter 2 - Service Nos.")
+    print("Enter 3 - Add name")
+    print("Enter 4 - Erase")
+    print("Enter 5 - Edit")
+    print("Enter 6 - Assign tone")
+    print("Enter 7 - Send b'card")
+    print("Enter 8 - Options")
+    print("Enter 9 - Speed dials")
+    print("Enter 10 - Voice tags")
+    print("Enter 0 - Back")
+    
+    
+
+    sub_menu = int(input("Enter Option:"))
+    if sub_menu == 8:
+        print("Option 1 - Type of view")
+        print("Option 2 - Memory status")
+        print("Option 3 - Back")
+        
+        choice = int(input("Input Choice: "))
+        if choice == 1:
+            print("Type of view")
+        elif choice == 2:
+            print("Memory status")
+
+
+user_choice = int(input("Input Choice: "))
+
+elif:
+    user_choice == 2:
+    print("Enter 1 - Write messages")
+    print("Enter 2 - Inbox")
+    print("Enter 3 - Outbox")
+    print("Enter 4 - Picture messages")
+    print("Enter 5 - Templates")
+    print("Enter 6 - Smileys")
+    print("Enter 7 - Message settings")
+    print("Enter 8 - Info service")
+    print("Enter 9 - Voice mailbox number")
+    print("Enter 10 - Service command editor")
+    print("Enter 0 - Back")
+    
+    
+
+    sub_menu = int(input("Enter Option:"))
+    if sub_menu == 7:
+        print("Option 1 - Type of view")
+        print("Option 2 - Memory status")
+        print("Option 3 - Back")
+        
+        choice = int(input("Input Choice: "))
+        if choice == 1:
+            print("Type of view")
+        elif choice == 2:
+            print("Memory status")
